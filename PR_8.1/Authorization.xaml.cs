2ed4010f6097ce0c93058884a381fd143289f135
D1sTc <143994071+D1sTc@users.noreply.github.com>
2024-06-24 17:11:21 +0300

    Add files via upload

---
+++ PR_8.1/Authorization.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PR_8._1
+{
+    /// <summary>
+    /// Логика взаимодействия для Authorization.xaml
+    /// </summary>
+    public partial class Authorization : Window
+    {
+        public Authorization()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Autorization_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+            MakingAnOrder winm = new MakingAnOrder();
+            winm.Show();
+
+            //// для администратора:
+            
+            //Close();
+            //MainMenu mm = new MainMenu();
+            //mm.Show();
+        }
+
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
